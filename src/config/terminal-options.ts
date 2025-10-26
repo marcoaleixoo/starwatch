6b53d1a90631ac5bbfa025964d6f9479940a32c0
Marco Aleixo <marco.aleixo@misterturing.com>
2025-10-26 01:23:05 -0300

    fix(terminal-options): update proximityRange to improve interaction thresholds

--- src/config/terminal-options.ts
+++ src/config/terminal-options.ts
@@ -8,7 +8,7 @@ export interface TerminalInteractionOptions {
 
 export const TERMINAL_INTERACTION_OPTIONS: TerminalInteractionOptions = {
   useRange: 3,
-  proximityRange: 2.05,
+  proximityRange: 3,
   disengageRange: 2.4,
   disengageGraceTicks: 6,
 };
